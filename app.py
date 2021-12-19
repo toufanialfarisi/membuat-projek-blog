7f6316a6d80ec6be43e806b494ec23cfa57f6c4b
toufanialfarisi <toufani.deeplearning@gmail.com>
2021-12-19 09:43:00 +0700

    update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, make_response, jsonify
 from flask_restful import Resource, Api 
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy 
+from functools import wraps
 
 # import library pendukung 
 import jwt 
@@ -20,6 +21,9 @@ filename = os.path.dirname(os.path.abspath(__file__))
 database = 'sqlite:///' + os.path.join(filename, 'db.sqlite')
 app.config['SQLALCHEMY_DATABASE_URI'] = database 
 
+# konfigurasi secret key 
+app.config['SECRET_KEY'] = "inirahasianegara"
+
 # membuat schema model database authentikasi (login, register)
 class AuthModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -35,6 +39,20 @@ class BlogModel(db.Model):
 
 # create model database ke dalam file db.sqlite
 db.create_all()
+
+# membuat decorator
+def butuh_token(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        token = request.args.get('datatoken') # http://127.0.0.1:5000/api/blog?datatoken=fkjsldkjflskdjflskd
+        if not token:
+            return make_response(jsonify({"msg":"Token nggak ada bro !"}), 401)
+        try:
+            jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+        except:
+            return make_response(jsonify({"msg":"Token nggak bener bro / invalid"}), 401)
+        return f(*args, **kwargs)
+    return decorator
 
 # membuat routing endpoint 
 # routing authentikasi 
@@ -62,17 +80,106 @@ class LoginUser(Resource):
 
         # query matching kecocokan data 
         # iterasi authModel 
-        query = [data.username for data in AuthModel.query.all()]  # list comprehension
-        if dataUsername in query :
+
+        # cek username dan password 
+        queryUsername = [data.username for data in AuthModel.query.all()]
+        queryPassword = [data.password for data in AuthModel.query.all()]
+        if dataUsername in queryUsername and dataPassword in queryPassword :
             # klo login sukses
-            return jsonify({"msg":"Login Sukses"})
+            # generate token authentikasi untuk user
+            token = jwt.encode(
+                {
+                    "username":queryUsername, "exp":datetime.datetime.utcnow() + datetime.timedelta(minutes=10)
+                }, app.config['SECRET_KEY'],  algorithm="HS256"
+            )
+            return make_response(jsonify({"msg":"Login Sukses", "token":token}), 200)
 
         # klo login gagal
         return jsonify({"msg":"Login gagal, silahkan coba lagi !!!"})
 
+
+# membuat endpoint untuk posting artikel baru bagi user yang sudah teregistrasi
+# endpoint ini akan diprotect 
+class TambahArtikel(Resource):
+    # akan diproteksi
+    @butuh_token
+    def post(self):
+        # tambah data artikel blog baru ke dalam database BlogModel
+        dataJudul = request.form.get('judul')
+        dataKonten = request.form.get('konten')
+        dataPenulis = request.form.get('penulis')
+        
+        data = BlogModel(judul=dataJudul, konten=dataKonten, penulis=dataPenulis)
+        db.session.add(data)
+        db.session.commit()
+        return{"msg":"Artikel berhasil ditambahkan !"}, 200
+
+    # melihat semua artikel yang telah dibuat/publish
+    # akan diproteksi
+    @butuh_token
+    def get(self):
+        # query data dari database blogmodel (query semua datanya)
+        dataQuery = BlogModel.query.all()
+        # lakukan looping datanya ==> list comprehension 
+        # list di dalamnya dict
+        output = [
+            {
+                "id":data.id,
+                "judul":data.judul,
+                "konten":data.konten,
+                "penulis":data.penulis
+            } for data in dataQuery
+        ] 
+
+        return make_response(jsonify(output), 200)
+
+# Routing untuk menghapus / mengedit data berdasarkan id 
+class UpdateDataById(Resource):
+    # mengedit data sehingga butuh parameter id
+    @butuh_token
+    def put(self, id):
+        # query database berdasarkan id
+        query = BlogModel.query.get(id)
+        
+        # get data dari form / multipart form (front end)
+        dataJudul = request.form.get('judul')
+        dataKonten = request.form.get('konten')
+        dataPenulis = request.form.get('penulis')
+
+        query.judul = dataJudul
+        query.konten = dataKonten
+        query.penulis = dataPenulis
+        db.session.commit()
+        return make_response(jsonify({"msg" : "Data berhasil di Update"}), 200)
+
+    # delete data sehingga butuh parameter id (hapus satu data)
+    @butuh_token
+    def delete(self, id):
+        # query database berdasarkan id
+        query = BlogModel.query.get(id)
+        db.session.delete(query)
+        db.session.commit()
+        return make_response(jsonify({"msg":"Data berhasil dihapus"}), 200)
+
+    # view data berdasarkan id  (menampilkan satu buah data)
+    @butuh_token
+    def get(self, id):
+        # query data berdasarkan id 
+        query = BlogModel.query.get(id) # satu data saja ==> tidak perlu iterasi/looping
+        output = {
+            "id":query.id,
+            "judul":query.judul,
+            "konten":query.konten,
+            "penulis":query.penulis
+        }
+        return make_response(jsonify(output), 200)
+
+        
 # inisiasi resource api 
 api.add_resource(RegisterUser, "/api/register", methods=["POST"])
 api.add_resource(LoginUser, "/api/login", methods=["POST"])
+api.add_resource(TambahArtikel, "/api/blog", methods=["GET", "POST"])
+api.add_resource(UpdateDataById, "/api/blog/<id>", methods=["PUT", "DELETE", "GET"])
 
 # jalankan aplikasi app.py 
 if __name__ == "__main__":
